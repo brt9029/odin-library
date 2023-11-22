037cd9c6725a23d2824c588544b9cd2eca4ed262
Bryant <brt9029@gmail.com>
2023-11-22 13:59:57 -0500

    added button to open form using dialog & button to close dialog box

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 const myLibrary = [];
 
-function Book() {
-
+function Book(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary () {
 
 }
+
+const dialog = document.querySelector('dialog');
+const dialogBtn = document.getElementsByClassName('add-book-btn');
+
+dialogBtn[0].addEventListener('click', () => {
+    dialog.showModal();
+});
+
+const closeDialog = document.getElementsByClassName('close-dialog');
+
+closeDialog[0].addEventListener('click', () => {
+    dialog.close();
+});
+
+const submitForm = document.getElementById('submit');
+
+submitForm.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log();
+})
